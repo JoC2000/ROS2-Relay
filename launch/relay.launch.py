fc2d3c1f06d535e9fd13b38ea46054f0f4b6a990
JoC2000 <jose.canales@utec.edu.pe>
2025-04-01 17:54:33 -0500

    launch file for package

---
+++ launch/relay.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import  Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="simple_relay",
+            executable="cpp_publisher",
+            name="cpp_pub"
+        ),
+        Node(
+            package="simple_relay",
+            executable="py_subscriber.py",
+            name="py_sub",
+            output="screen",
+        ),
+    ])
